e5f4ecd987f9bd5035283f938dc35e43e37d4758
Thiago Rodrigues <thiagothogs@gmail.com>
2020-05-31 21:12:53 -0300

    corrigido problema de animação em main.css

--- website/assets/js/iframe.js
+++ website/assets/js/iframe.js
@@ -4,10 +4,16 @@ const btnCad = document.getElementById("btncad")
 const btnMail = document.getElementById("btnmail")
 const altcssMail = document.getElementById("inner-form-mail")
 const altcssCad = document.getElementById("inner-form-cad")
+const mailForm = document.getElementById('inner-form-mail')
+const cadForm = document.getElementById('inner-form-cad')
+const divForm = document.getElementById('div-form')
+
 
 btnMail.onclick = function (e) {
     e.preventDefault()
-    
+
+    cadForm.remove()
+
     if (altcssCad.classList.contains("hide-cad") && (altcssMail.classList.contains("hide-mail") )) {
 
         if (altcssMail.classList.contains("hide-mail")){
@@ -20,13 +26,17 @@ btnMail.onclick = function (e) {
             altcssMail.classList.remove("hide-mail")
             altcssCad.classList.add("hide-cad")
         } else{
-            altcssMail.classList.add("hide-mail")
-        }
+            altcssMail.classList.add("hide-mail")}
     }
+
+    divForm.appendChild(cadForm)
 }
+
 
 btnCad.onclick = function (e) {
     e.preventDefault()
+
+    mailForm.remove()
 
     if (altcssMail.classList.contains("hide-mail") && (altcssCad.classList.contains("hide-cad") )) {
 
@@ -36,11 +46,13 @@ btnCad.onclick = function (e) {
             altcssCad.classList.add("hide-cad")}
 
     } else{
+
         if (altcssCad.classList.contains("hide-cad") ){
             altcssCad.classList.remove("hide-cad")
             altcssMail.classList.add("hide-mail")
         } else{
-            altcssCad.classList.add("hide-cad")
-        }
+            altcssCad.classList.add("hide-cad")}
     }
+
+    divForm.appendChild(mailForm)
 }
