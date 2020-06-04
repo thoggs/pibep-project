5eb2c5f6608658fc1c7a4f24f67aa7d3e8d30ef5
Thiago Rodrigues <thiagothogs@gmail.com>
2020-06-04 15:32:16 -0300

    refatoração de codigo

--- website/assets/js/iframe.js
+++ website/assets/js/iframe.js
@@ -1,23 +1,22 @@
 
 
 const btnHeader = document.getElementById('btnHeader')
-const el = document.getElementById('upArrow"')
-const btnCancel = document.getElementById('btnCancel')
-
+const arrowElement = document.getElementById('upArrow"')
+const btnCancarrowElement = document.getElementById('btnCancel')
 
 
 btnHeader.onclick = function (e) {
     e.preventDefault()
 
-    el.style.setProperty('opacity', '1')
-    el.style.setProperty('visibility', 'visible')
+    arrowElement.style.setProperty('opacity', '1')
+    arrowElement.style.setProperty('visibility', 'visible')
     btnHeader.style.setProperty('display', 'none')
 }
 
-btnCancel.onclick = function (e) {
+btnCancarrowElement.onclick = function (e) {
     e.preventDefault()
 
-    el.style.setProperty('opacity', '0')
-    el.style.setProperty('visibility', 'hidden')
+    arrowElement.style.setProperty('opacity', '0')
+    arrowElement.style.setProperty('visibility', 'hidden')
     btnHeader.style.setProperty('display', 'flex')
 }
